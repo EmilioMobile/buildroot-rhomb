084c8d0d6a8251a0be9b81dac9354a742a09dca7
Android Linux Media <android.linuxmedia@gmail.com>
2017-03-14 18:52:13 +0530

    feature(rhomb): u-boot support for GAIA board
    This commit add u-boot support for GAIA board in buildroot. It also updates
    "prepare_sd.sh" script to flash generated u-boot.bin binary along with prebuild
    bl1, bl2, tszw binaires which are essential for the board boot. In addition to
    that "build.sh" is updated for u-boot support.
    
    Signed-off-by: Android Linux Media <android.linuxmedia@gmail.com>

--- build.sh
+++ build.sh
@@ -5,24 +5,75 @@ arg="$1"
 function print_help () {
 	echo "####################################################"
 	echo "To build complete buildroot and generate rootfs.tar"
-	echo "sudo ./build.sh"
+	echo "sudo ./build.sh all"
 	echo ""
 	echo "To rebuild linux with updated changes"
 	echo "sudo ./build.sh linux"
+	echo ""
+	echo "To rebuild uboot with updated changes"
+	echo "sudo ./build.sh uboot"
+	echo ""
+	echo "To generate rootfs.tar"
+	echo "sudo ./build.sh rootfs"
+	echo ""
+	echo "To clean all the directories"
+	echo "sudo ./build.sh distclean"
+	echo "WARNING: Above command will delete all the " \
+	"downlaoded packages and restart all the things from " \
+        "scratch"
+	echo ""
+	echo "To get help of build script commands"
+	echo "sudo ./build.sh help"
 	echo "####################################################"
+}
+
+function linux_build () {
+	cd output/build/linux-odroid-4.8.y
+	sudo git pull
+	cd -
+	sudo make linux-rebuild
+	sudo make linux-install
+}
+
+function uboot_build () {
+	cd output/build/uboot-odroid-v2015.10
+	sudo git pull
+	cd -
+	sudo make uboot-rebuild
+	sudo make uboot-install
+}
+
+function apply_buildroot_defconfig () {
+	sudo make rhomb_defconfig
+}
+
+function rootfs_build () {
+	apply_buildroot_defconfig
+	sudo make
+}
+
+function all_dir_clean () {
+	sudo make distclean
 }
 
 case $arg in
 	"linux")
-		cd output/build/linux-odroid-4.8.y
-		sudo git pull
-		cd -
-		sudo make linux-rebuild
-		sudo make linux-install
+		linux_build
+	;;
+	"uboot")
+		uboot_build
+	;;
+	"rootfs")
+		rootfs_build
 	;;
 	"all")
-		sudo make rhomb_defconfig
-		sudo make
+		apply_buildroot_defconfig
+		uboot_build
+		linux_build
+		rootfs_build
+	;;
+	"distclean")
+		all_dir_clean
 	;;
 	"help")
 		print_help
--- prepare_sd.sh
+++ prepare_sd.sh
@@ -1,25 +1,34 @@
 #/bin/bash
-echo "Enter mounted partition of sd card node name"
-echo "example: /dev/sdb1, /dev/mmcblk0p1"
-echo "         Just give parition name \"sdb1\", \"mmcblk0p1\" etc"
-read node
-if [ -b "/dev/$node" ]; then
-	echo "node found"
-	sudo umount /dev/$node
-	sudo mkfs.ext4 /dev/$node
+if [ -z $1 ]
+then
+	echo "usage:./prepare_sd.sh <SD Reader's device file>"
+	echo "example: sdb, sdd, mmcblk0 etc"
+	exit 0
+fi
+
+cd output/build/uboot-odroid-v2015.10/sd_fuse
+sudo ./sd_fusing.sh /dev/$1
+sync
+cd -
+if [ -b "/dev/$11" ]; then
+	echo "partition found"
+	sudo umount /dev/$11
+	sudo mkfs.ext4 /dev/$11
 	sudo mkdir -p /mnt/tmp
 	sudo mount /dev/sdb1 /mnt/tmp
 	sudo tar -xf output/images/rootfs.tar -C /mnt/tmp/
 	sudo cp output/images/zImage.exynos4412-rhomb-expansion /mnt/tmp/zImage
 	sync
-	sudo umount /dev/$node
+	sudo umount /dev/$11
 	echo "SD card prepared"
 	echo "Insert sd card into board"
-	echo "Set below bootargs in board and bootloader prompt and then boot"
+	echo "Set below bootcmd at bootlaoder prompt"
+	echo "setenv bootcmd 'ext4load mmc 0:1 0x40008000 zImage;bootm 0x40008000'"
+	echo "Set below bootargs at bootloader prompt and then boot"
 	echo "setenv bootargs 'console=tty1 console=ttySAC1,115200 mem=1023M root=/dev/mmcblk0p1 rootwait rw'"
 	echo "Good luck!"
 else
-	echo "node not found please check in /dev/<> directory"
+	echo "partition not found please check in /dev/<> directory"
 	echo "please check dmesg for verification"
 fi
 
